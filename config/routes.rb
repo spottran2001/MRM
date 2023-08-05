74c18dc5a230a273d153e29f9572ee4a600f2dc4
lethnhan <cloudpon0809@gmail.com>
2023-08-05 13:23:09 +0700

    f_login

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/login/', to: 'home#page_login' 
+  get '/page/', to: 'home#home_page' 
 end
