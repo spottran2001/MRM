2113a393ac8293e85abec43e2565a209c9bb1d90
spot <spottran2001@gmail.com>
2023-09-04 18:10:24 +0700

    Create seed data

--- app/controllers/faculty_controller.rb
+++ app/controllers/faculty_controller.rb
@@ -3,6 +3,8 @@ class FacultyController < ApplicationController
 
   def index
     @faculties = Faculty.all.order(:created_at)
+    Faculty.create!(name: "Ky Thuat Phan Men")
+    Subject.create!(name: "Cong Nghe Thong Tin")
   end
 
   def create
