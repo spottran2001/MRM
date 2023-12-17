810c0d23bcd22bfabdd4725387a0dee536a5dbcf
spot <spot@vinova.com.sg>
2023-12-17 13:00:06 +0700

    Fix - config mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   # mailer
   # config.action_mailer.default_url_options = {host: "localhost", port: 3000}
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   
   config.action_mailer.delivery_method = :smtp
