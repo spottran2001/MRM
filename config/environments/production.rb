898e2809e893987b05facebbada0bb49a8691417
spot <spot@vinova.com.sg>
2023-12-17 12:31:19 +0700

    Fix - config mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # mailer
-  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  # config.action_mailer.default_url_options = {host: "localhost", port: 3000}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   
