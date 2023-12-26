aaa11e4fbf4ba02e45dc1a2d0417dfca6c3ffaa1
spot <spot@vinova.com.sg>
2023-12-26 14:31:35 +0700

    Fix email cron production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+job_type :rake,    "cd :path && :environment_variable=:environment SECRET_KEY_BASE=production_test_key bundle exec rake :task --silent :output"
 set :output, {error: "log/cron_error_log.log", standard: "log/cron_log.log"}
 
 every 5.minute do # Many shortcuts available: :hour, :day, :month, :year, :reboot
